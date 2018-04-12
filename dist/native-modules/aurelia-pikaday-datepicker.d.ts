407958f31d2aacc22595d9bd1f305ed29b57e683
Axel Verstappen <axel.verstappen@gmail.com>
2018-04-12 08:53:11 +0200

    Update dist build

--- dist/native-modules/aurelia-pikaday-datepicker.d.ts
+++ dist/native-modules/aurelia-pikaday-datepicker.d.ts
@@ -1,14 +1,2 @@
-/// <reference types="pikaday" />
-import * as Pikaday from 'pikaday';
 export declare class AureliaPikadayDatepicker {
-    private element;
-    value: string;
-    placeholder: string;
-    config: Pikaday.PikadayOptions;
-    private picker;
-    private i18n;
-    constructor(element: Element);
-    attached(): void;
-    keydown(e: KeyboardEvent): boolean;
-    private setConfig();
 }
--- dist/system/aurelia-pikaday-datepicker.js
+++ dist/system/aurelia-pikaday-datepicker.js
@@ -1,126 +1,13 @@
-System.register(["aurelia-framework", "pikaday", "moment"], function (exports_1, context_1) {
+System.register([], function (exports_1, context_1) {
     "use strict";
-    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-        return c > 3 && r && Object.defineProperty(target, key, r), r;
-    };
-    var __metadata = (this && this.__metadata) || function (k, v) {
-        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-    };
     var __moduleName = context_1 && context_1.id;
-    var aurelia_framework_1, Pikaday, moment, AureliaPikadayDatepicker;
+    var AureliaPikadayDatepicker;
     return {
-        setters: [
-            function (aurelia_framework_1_1) {
-                aurelia_framework_1 = aurelia_framework_1_1;
-            },
-            function (Pikaday_1) {
-                Pikaday = Pikaday_1;
-            },
-            function (moment_1) {
-                moment = moment_1;
-            }
-        ],
+        setters: [],
         execute: function () {
             AureliaPikadayDatepicker = (function () {
-                function AureliaPikadayDatepicker(element) {
-                    this.element = element;
-                    this.placeholder = '';
-                    this.config = {};
-                    this.i18n = {
-                        previousMonth: 'Vorige Maand',
-                        nextMonth: 'Volgende Maand',
-                        months: [
-                            'Januari',
-                            'Februari',
-                            'Maart',
-                            'April',
-                            'Mei',
-                            'Juni',
-                            'Juli',
-                            'Augustus',
-                            'September',
-                            'Oktober',
-                            'November',
-                            'December'
-                        ],
-                        weekdays: [
-                            'Zondag',
-                            'Maandag',
-                            'Dinsdag',
-                            'Woensdag',
-                            'Donderdag',
-                            'Vrijdag',
-                            'Zaterdag'
-                        ],
-                        weekdaysShort: [
-                            'Zo',
-                            'Ma',
-                            'Di',
-                            'Wo',
-                            'Do',
-                            'Vr',
-                            'Za'
-                        ]
-                    };
+                function AureliaPikadayDatepicker() {
                 }
-                AureliaPikadayDatepicker.prototype.attached = function () {
-                    this.setConfig();
-                    this.picker = new Pikaday(this.config);
-                    if (this.config.defaultDate) {
-                        this.picker.setDate(this.config.defaultDate);
-                    }
-                };
-                AureliaPikadayDatepicker.prototype.keydown = function (e) {
-                    var key = e.which;
-                    if (key >= 48 && key <= 57 || key === 191 || key === 8 || key === 189) {
-                        if (this.picker.isVisible()) {
-                            this.picker.hide();
-                        }
-                        return true;
-                    }
-                    else {
-                        return false;
-                    }
-                };
-                AureliaPikadayDatepicker.prototype.setConfig = function () {
-                    this.config.field = this.element.querySelector('input');
-                    if (!this.config.i18n) {
-                        this.config.i18n = this.i18n;
-                    }
-                    if (!this.config.firstDay) {
-                        this.config.firstDay = 1;
-                    }
-                    this.config.format = 'DD/MM/YYYY';
-                    this.config.toString = function (date, format) {
-                        return moment(date).format(format);
-                    };
-                    this.config.parse = function (dateString, format) {
-                        var parts = dateString.split(/\/|-|\./);
-                        var day = Math.floor(parts[0]);
-                        var month = Math.floor(parts[1] - 1);
-                        var year = Math.floor(parts[2]);
-                        return new Date(year, month, day);
-                    };
-                };
-                __decorate([
-                    aurelia_framework_1.bindable({ defaultBindingMode: aurelia_framework_1.bindingMode.twoWay }),
-                    __metadata("design:type", String)
-                ], AureliaPikadayDatepicker.prototype, "value");
-                __decorate([
-                    aurelia_framework_1.bindable,
-                    __metadata("design:type", String)
-                ], AureliaPikadayDatepicker.prototype, "placeholder");
-                __decorate([
-                    aurelia_framework_1.bindable,
-                    __metadata("design:type", Object)
-                ], AureliaPikadayDatepicker.prototype, "config");
-                AureliaPikadayDatepicker = __decorate([
-                    aurelia_framework_1.inject(Element),
-                    __metadata("design:paramtypes", [Element])
-                ], AureliaPikadayDatepicker);
                 return AureliaPikadayDatepicker;
             }());
             exports_1("AureliaPikadayDatepicker", AureliaPikadayDatepicker);
